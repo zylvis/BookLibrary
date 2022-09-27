5b5d7880514b088cbafdca89ee5bf1115951a8c4
Zilvinas Juodis <zilvinoj@gmail.com>
2022-09-27 08:28:23 +0300

    update BookDTO

--- BookLibraryAPI/Models/Dto/BookDTO.cs
+++ BookLibraryAPI/Models/Dto/BookDTO.cs
@@ -13,6 +13,10 @@ namespace BookLibraryAPI.Models.Dto
         public double ISBN { get; set; }
         [Required]
         public int Year { get; set; }
+        public string Publisher { get; set; }
+        public string Genre { get; set; }
+        public string AvailableStatus { get; set; }
+        public bool Reserved { get; set; }
 
     }
 }
